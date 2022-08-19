4ae88fa2dc3dc1f8f4d49823d0e913986905fe23
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-08-19 19:59:01 +0600

    done pin matcher

--- js/app.js
+++ js/app.js
@@ -1,27 +1,64 @@
-
-
-
-//function for random pin
-
-const randomPin = ()=>{
-    const  pin = Math.round(Math.random() * 10000);
-    const  pinStr = pin+'' 
-
-    if (pinStr.length === 4) {
-        return pin
-    }else{
-        
-        return randomPin()
+function getPin(){
+    const pin = generatePin();
+    const pinString = pin + '';
+    if(pinString.length === 4){
+        return pin;
     }
-    
-    
+    else{
+        // console.log('pin not 3 digit found', pin);
+        return getPin();
+    }
 }
 
-// set pin number in input fileld
+function generatePin(){
+    const random = Math.round(Math.random()*10000);
+    return random;
+}
 
-document.getElementById('genarate').addEventListener('click',()=>{
+document.getElementById('generate-pin').addEventListener('click', function(){
+    const pin = getPin();
+    // display Pin
+    const displayPinField = document.getElementById('display-pin');
+    displayPinField.value = pin;
+});
 
-    const pin = randomPin()
+document.getElementById('calculator').addEventListener('click', function(event){
+    const number = event.target.innerText;
+    const typedNumberField = document.getElementById('typed-numbers');
+    const previousTypedNumber = typedNumberField.value;
+    if(isNaN(number)){
+        if(number === 'C'){
+            typedNumberField.value = '';
+        }
+        else if (number === '<'){
+            const digits = previousTypedNumber.split('');
+            digits.pop();
+            const remainingDigits = digits.join('');
+            typedNumberField.value = remainingDigits;
+        }
+    }
+    else{
+        const newTypedNumber = previousTypedNumber + number;
+        typedNumberField.value = newTypedNumber;
+    }
+})
 
-   document.getElementById('display-pin').value = pin
+document.getElementById('verify-pin').addEventListener('click', function(){
+    const displayPinField = document.getElementById('display-pin');
+    const currentPin = displayPinField.value;
+
+    const typedNumberField = document.getElementById('typed-numbers');
+    const typedNumber = typedNumberField.value;
+
+    const pinSuccessMessage = document.getElementById('pin-success');
+    const pinFailureMessage = document.getElementById('pin-failure');
+
+    if(typedNumber === currentPin && typedNumber.length === 4){
+        pinSuccessMessage.style.display = 'block';
+        pinFailureMessage.style.display = 'none';
+    }
+    else{
+        pinFailureMessage.style.display = 'block';
+        pinSuccessMessage.style.display = 'none';
+    }
 })
